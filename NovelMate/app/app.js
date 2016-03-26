07341714dc304b182dbdd42b11e06df03cb37cde
Brian Rodham <brian.j.rodham@gmail.com>
2016-03-26 16:44:02 -0600

    Initial Commit

---
+++ NovelMate/app/app.js
@@ -0,0 +1,11 @@
+﻿
+    var app = angular.module('novelmate', ['ngRoute', 'ngAnimate']);
+
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'WriterController',
+                templateUrl: 'app/views/writer.html'
+            })
+            .otherwise({ redirectTo: '/' });
+    });
---
+++ NovelMate/app/controllers/writerController.js
@@ -0,0 +1,62 @@
+﻿app.controller('WriterController', ['$scope', function ($scope) {
+    $scope.content = ""; // Current chapter text
+
+    function init() {
+
+
+       /* CKEDITOR.replace('editor');
+        var editor = CKEDITOR.instances['editor'];
+
+        editor.resize($("#plotform").width(), $("#plotform").height());*/
+
+        console.log("Alert");
+
+        CKEDITOR.editorConfig = function (config) {
+            config.language = 'es';
+            config.uiColor = '#F7B42C';
+            config.height = 300;
+            config.toolbarCanCollapse = true;
+
+            config.toolbarGroups = [
+               { name: 'clipboard', groups: ['clipboard', 'undo'] },
+               { name: 'editing', groups: ['find', 'selection', 'spellchecker'] },
+               { name: 'links' },
+               { name: 'insert' },
+               { name: 'tools' },
+               { name: 'document', groups: ['mode', 'document', 'doctools'] },
+               { name: 'others' },
+               { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+               { name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi'] },
+               { name: 'styles' },
+               { name: 'colors' },
+               { name: 'about' }
+            ];
+
+        };
+
+        CKEDITOR.replace('editor');
+
+    }
+
+    init();
+
+}]);
+
+/*(function () {
+    var WriterController = function ($scope) {
+        function init() {
+            $(".writer-nav").css({ 'height': ($(".editor").height() + 'px') });
+
+            $('.writer-nav').tinyscrollbar();
+
+            CKEDITOR.replace('editor');
+            var editor = CKEDITOR.instances['editor'];
+
+            editor.resize($("#plotform").width(), $("#plotform").height());
+        }
+
+        init();
+
+    };
+    angular.module('novelmate').controller('WriterController', WriterController);
+}());*/
